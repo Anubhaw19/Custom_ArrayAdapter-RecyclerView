3bee11c8bfe679003e21411e5210306df080de3d
Anubhaw Sharma <anubhawsharma99@gmail.com>
2019-12-02 01:20:59 +0530

    added images in the layout.

--- app/src/main/java/com/example/learn/Word.java
+++ app/src/main/java/com/example/learn/Word.java
@@ -3,10 +3,21 @@ package com.example.learn;
 public class Word {
 private  String mMiwokTranslation;
 private String mDefaultTranslation;
+private int mImageResourceId;
 public Word(String defaultTranslation,String miwokTranslation)
 {
     mDefaultTranslation=defaultTranslation;
     mMiwokTranslation=miwokTranslation;
+}
+    public Word(String defaultTranslation,String miwokTranslation,int ImageResourceId)
+    {
+        mDefaultTranslation=defaultTranslation;
+        mMiwokTranslation=miwokTranslation;
+        mImageResourceId=ImageResourceId;
+    }
+public  int getImageResourceId()
+{
+    return  mImageResourceId;
 }
 public String getMiwokTranslation(){
     return mMiwokTranslation;
--- app/src/main/java/com/example/learn/second.java
+++ app/src/main/java/com/example/learn/second.java
@@ -23,21 +23,21 @@ public class second extends AppCompatActivity {
       @SuppressLint("WrongViewCast") ListView list=(ListView) findViewById(R.id.list); //we can declare this ListView in For loop also.,that will also work fine
         //creating a ArrayList
         ArrayList<Word> words=new ArrayList<Word>();
-        words.add(new Word("one","1"));
-        words.add(new Word("two","2"));
-        words.add(new Word("three","3"));
-        words.add(new Word("four","4"));
-        words.add(new Word("five","5"));
-        words.add(new Word("six","6"));
-        words.add(new Word("seven","7"));
-        words.add(new Word("eight","8"));
-        words.add(new Word("nine","9"));
-        words.add(new Word("ten","10"));
-        words.add(new Word("eleven","11"));
-        words.add(new Word("twelve","12"));
-        words.add(new Word("thirteen","13"));
-        words.add(new Word("fourteen","14"));
-        words.add(new Word("fifteen","15"));
+        words.add(new Word("one","1",R.drawable.p1));
+        words.add(new Word("two","2",R.drawable.p2));
+        words.add(new Word("three","3",R.drawable.p3));
+        words.add(new Word("four","4",R.drawable.p4));
+        words.add(new Word("five","5",R.drawable.p5));
+        words.add(new Word("six","6",R.drawable.p6));
+        words.add(new Word("seven","7",R.drawable.p7));
+        words.add(new Word("eight","8",R.drawable.p8));
+        words.add(new Word("nine","9",R.drawable.p9));
+        words.add(new Word("ten","10",R.drawable.p11));
+        words.add(new Word("eleven","11",R.drawable.p11));
+        words.add(new Word("twelve","12",R.drawable.p12));
+        words.add(new Word("thirteen","13",R.drawable.p13));
+        words.add(new Word("fourteen","14",R.drawable.p13));
+        words.add(new Word("fifteen","15",R.drawable.p12));
 
 
         for(int index=0;index<words.size();index++)
@@ -48,11 +48,11 @@ public class second extends AppCompatActivity {
 
 //            TextView text=new TextView(this);
 //            text.setText(word.get(index));
-//            linear.addView(text);
+//            list.addView(text);
 
             //ArrayAdapter<Word> adapter=new ArrayAdapter<Word>(this,R.layout.list_item,words);
 
-            //here [WordAdapter] class extends from [ArrayAdapter] class and overrided a function [getView],so that it can show more than one TextView ,orto create a complex LayOut
+            //here [WordAdapter] class extends from [ArrayAdapter] class and overrided a function [getView],so that it can show more than one TextView ,or to create a complex LayOut
             WordAdapter adapter=new WordAdapter(this,words);//constructor of [WordAdapter] is taking only two attributes,i.e[context,Source]
             list.setAdapter(adapter);
         }
